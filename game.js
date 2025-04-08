e6b5cb35207119851ee33195fa1fd19a7176fd14
Raunak Pratap <raunakpratap428@gmail.com>
2025-04-08 22:19:59 +0530

    Reduced win notification duration to 1s

--- game.js
+++ game.js
@@ -251,7 +251,7 @@ document.addEventListener('DOMContentLoaded', () => {
       setTimeout(() => {
         notif.classList.remove('show');
         notif.classList.add('hidden');
-      }, 1000);
+      }, 800);
     }
   
     function pollForNotification() {
